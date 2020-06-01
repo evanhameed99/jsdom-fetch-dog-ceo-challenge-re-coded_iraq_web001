2b6c2fca9946eaa5c7c45817511a1d74489e871f
Evan hameed <evanhameed9966@gmail.com>
2020-06-01 07:13:47 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
 console.log('%c HI', 'color: firebrick');
 
+const imgUrl = "https://dog.ceo/api/breeds/image/random/4";
 
+// fetching images from the api
+function fetchImagesFromApi(){
+
+fetch(imgUrl);
+
+
+}
 
 
 window.addEventListener('DOMContentLoaded', ()=>{
-  
+
 })
